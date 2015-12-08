f76c8aea44fd8e7c295a38a1b6a68bbee9b0819c
Lee Tagg <mahkram@me.com>
2015-12-08 17:01:55 +1000

    test set & localise

--- test/test.js
+++ test/test.js
@@ -13,29 +13,12 @@ exports.testGetLanguage = function(test) {
   test.equals(testable.getLanguage(), 'en', "unexpected value returned");
   test.done();
 }
-//
-//exports.testLocalise = function(test) {
-//    testable.on('update', function() {
-//        test.equals(testable.localise('boo'), 'boo', "unexpected value returned");
-//        test.done();
-//    }.bind(testable))
-//    testable.setLanguage('en');
-//}
 
-
-exports.testSetLanguage = function(test) {
+exports.testSetLocalise = function(test) {
     testable.on('update', function() {
-        test.ok(true, "update language callback was not triggered");
+        test.equals(testable.localise('Hello'), 'こんにちは', "unexpected value returned")
         test.done();
     }.bind(this))
     testable.setLanguage('jp');
-}
-
-exports.testChangeLanguage = function(test) {
-    testable.on('update', function() {
-        test.ok(true, "update language callback was not triggered");
-        test.done();
-    }.bind(this))
-    testable.setLanguage('cz');
 }
 
