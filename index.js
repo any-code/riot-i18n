71d2b04f0de3ccfe602708290470b0e5ecd360a6
Lee Tagg <mahkram@me.com>
2016-01-04 08:50:25 +1000

    UMD, _entities
    uses UMD from https://gist.github.com/qrb/e4cbbef4e062109277f8 and
    updates _entitiies to _entities

--- index.js
+++ index.js
@@ -1,69 +1,68 @@
-(function(def){
-    def(['riot'], function(riot) {
+function Module(exports, riot) {
+    var DEFAULT_LANG = 'en';
 
-        var DEFAULT_LANG = 'en';
-
-        function I18n() {
-            this._entitiies = {}
-            this._default = DEFAULT_LANG
-            this._language = this._default
-            var obs = riot.observable()
-            this.on = obs.on
-            this.off = obs.off
-            this.trigger = obs.trigger
-            this.on('lang', this.setLanguage)
-        }
-
-        I18n.prototype.dictionary = function(dict) {
-            this._entities = dict;
-        }
-
-        I18n.prototype.defaultLanguage = function(lang) {
-            this._default = lang;
-        }
-
-        I18n.prototype.localise = function(key) {
-            if (!this._entities[this._language] || !this._entities[this._language][key]) {
-                // When a language or translation is not
-                // provided, just return the original text.
-                return key
-            }
-
-            // return the language substitue for the original text
-            return this._entities[this._language][key]
-        }
-
-        I18n.prototype.setLanguage = function(lang) {
-            this._language = lang || this._default
-            this.trigger('update')
-        }
-
-        I18n.prototype.getLanguage = function() {
-            return this._language
-        }
-
-        var i18n = new I18n()
-
-        riot.mixin('i18n', { i18n: i18n })
-
-        //
-        //
-        // BEGIN RIOT TAGS
-        // END RIOT TAGS
-        //
-        //
-
-        return i18n;
-    });
-}( (function(darr, name){
-    if (typeof require === 'undefined') {
-        return function (deps, factory) { this[name] = factory.apply(this, darr.map(function(arg) { return window[arg] })); }
-    } else if (typeof exports === 'undefined') {
-        return function (deps, factory) { define(name, deps, factory); }
-    } else {
-        require('riot'); // must find a solution to why require needs to be called with primitive
-        return function (deps, factory) { module.exports = factory.apply(this, deps.map(require)); }
+    function I18n() {
+        this._entities = {}
+        this._default = DEFAULT_LANG
+        this._language = this._default
+        var obs = riot.observable()
+        this.on = obs.on
+        this.off = obs.off
+        this.trigger = obs.trigger
+        this.on('lang', this.setLanguage)
     }
-})(['riot'], 'i18n') ));
 
+    I18n.prototype.dictionary = function(dict) {
+        this._entities = dict;
+    }
 
+    I18n.prototype.defaultLanguage = function(lang) {
+        this._default = lang;
+    }
+
+    I18n.prototype.localise = function(key) {
+        if (!this._entities[this._language] || !this._entities[this._language][key]) {
+            // When a language or translation is not
+            // provided, just return the original text.
+            return key
+        }
+
+        // return the language substitue for the original text
+        return this._entities[this._language][key]
+    }
+
+    I18n.prototype.setLanguage = function(lang) {
+        this._language = lang || this._default
+        this.trigger('update')
+    }
+
+    I18n.prototype.getLanguage = function() {
+        return this._language
+    }
+
+    var i18n = new I18n()
+    for (property in i18n) {
+        exports[property] = i18n[property];
+    }
+    riot.mixin('i18n', { i18n: i18n })
+
+    //
+    //
+    // BEGIN RIOT TAGS
+
+    // END RIOT TAGS
+    //
+    //
+}
+
+Module.prototype.dependencies = ['riot'] // array of dependencies
+Module.prototype.global = ["i18n", "riot-i18n"]; // array of global identifiers for module, useful if module can be required using a hyphenated-name
+
+// Module UMD Loader
+(function (g, f) {
+    var d=Module.prototype.dependencies,gn=Module.prototype.global
+    if (typeof define==='function'&&define.amd){define(['exports'].concat(d||[]),f)}else if(typeof exports==='object'&&
+        typeof exports.nodeName!=='string'){f.apply(this,[exports].concat(d?d.map(require):[]))}else{if(typeof gn==='string'
+    )gn=[gn];g[gn[0]]={};gn.splice(1).map(function(d){g[d]=g[gn[0]]});f.apply(this, [g[gn[0]]].concat(d?d.map(function(d
+    ){return g[d]}):[]))}
+}(this, Module));
--- riot-i18n.js
+++ riot-i18n.js
@@ -1,54 +1,54 @@
-(function(def){
-    def(['riot'], function(riot) {
+function Module(exports, riot) {
+    var DEFAULT_LANG = 'en';
 
-        var DEFAULT_LANG = 'en';
+    function I18n() {
+        this._entities = {}
+        this._default = DEFAULT_LANG
+        this._language = this._default
+        var obs = riot.observable()
+        this.on = obs.on
+        this.off = obs.off
+        this.trigger = obs.trigger
+        this.on('lang', this.setLanguage)
+    }
 
-        function I18n() {
-            this._entitiies = {}
-            this._default = DEFAULT_LANG
-            this._language = this._default
-            var obs = riot.observable()
-            this.on = obs.on
-            this.off = obs.off
-            this.trigger = obs.trigger
-            this.on('lang', this.setLanguage)
+    I18n.prototype.dictionary = function(dict) {
+        this._entities = dict;
+    }
+
+    I18n.prototype.defaultLanguage = function(lang) {
+        this._default = lang;
+    }
+
+    I18n.prototype.localise = function(key) {
+        if (!this._entities[this._language] || !this._entities[this._language][key]) {
+            // When a language or translation is not
+            // provided, just return the original text.
+            return key
         }
 
-        I18n.prototype.dictionary = function(dict) {
-            this._entities = dict;
-        }
+        // return the language substitue for the original text
+        return this._entities[this._language][key]
+    }
 
-        I18n.prototype.defaultLanguage = function(lang) {
-            this._default = lang;
-        }
+    I18n.prototype.setLanguage = function(lang) {
+        this._language = lang || this._default
+        this.trigger('update')
+    }
 
-        I18n.prototype.localise = function(key) {
-            if (!this._entities[this._language] || !this._entities[this._language][key]) {
-                // When a language or translation is not
-                // provided, just return the original text.
-                return key
-            }
+    I18n.prototype.getLanguage = function() {
+        return this._language
+    }
 
-            // return the language substitue for the original text
-            return this._entities[this._language][key]
-        }
+    var i18n = new I18n()
+    for (property in i18n) {
+        exports[property] = i18n[property];
+    }
+    riot.mixin('i18n', { i18n: i18n })
 
-        I18n.prototype.setLanguage = function(lang) {
-            this._language = lang || this._default
-            this.trigger('update')
-        }
-
-        I18n.prototype.getLanguage = function() {
-            return this._language
-        }
-
-        var i18n = new I18n()
-
-        riot.mixin('i18n', { i18n: i18n })
-
-        //
-        //
-        //BEGIN RIOT TAGS
+    //
+    //
+    //BEGIN RIOT TAGS
 riot.tag2('i1-8n', '<span name="localised"></span> <span name="original" class="original"><yield></yield></span>', 'i1-8n,[riot-tag="i1-8n"] { display: inline-block; } i1-8n .original,[riot-tag="i1-8n"] .original { display: none; }', '', function(opts) {
         this.mixin('i18n')
 
@@ -61,20 +61,18 @@ riot.tag2('i1-8n', '<span name="localised"></span> <span name="original" class="
         })
 });
         //END RIOT TAGS
-        //
-        //
+    //
+    //
+}
 
-        return i18n;
-    });
-}( (function(darr, name){
-    if (typeof require === 'undefined') {
-        return function (deps, factory) { this[name] = factory.apply(this, darr.map(function(arg) { return window[arg] })); }
-    } else if (typeof exports === 'undefined') {
-        return function (deps, factory) { define(name, deps, factory); }
-    } else {
-        require('riot'); // must find a solution to why require needs to be called with primitive
-        return function (deps, factory) { module.exports = factory.apply(this, deps.map(require)); }
-    }
-})(['riot'], 'i18n') ));
+Module.prototype.dependencies = ['riot'] // array of dependencies
+Module.prototype.global = ["i18n", "riot-i18n"]; // array of global identifiers for module, useful if module can be required using a hyphenated-name
 
-
+// Module UMD Loader
+(function (g, f) {
+    var d=Module.prototype.dependencies,gn=Module.prototype.global
+    if (typeof define==='function'&&define.amd){define(['exports'].concat(d||[]),f)}else if(typeof exports==='object'&&
+        typeof exports.nodeName!=='string'){f.apply(this,[exports].concat(d?d.map(require):[]))}else{if(typeof gn==='string'
+    )gn=[gn];g[gn[0]]={};gn.splice(1).map(function(d){g[d]=g[gn[0]]});f.apply(this, [g[gn[0]]].concat(d?d.map(function(d
+    ){return g[d]}):[]))}
+}(this, Module));
